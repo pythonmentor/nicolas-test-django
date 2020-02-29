82b9a9c1302f159d3258e20ce94d1c6571f69a84
NicolasDutour <dutourn99@gmail.com>
2020-02-29 17:35:47 +0100

    inject json file into sqlite database and update index.html template

--- company/views.py
+++ company/views.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from .models import Company
+import json
+
+with open('mock_data.json', 'r') as f:
+    companies = json.load(f)
+    for company_data in companies:
+        company = Company.create(**company_data)
+
 
 def index(request):
-	return render(request, 'index.html', {})
+    companies = Company.objects.all()
 
+    context = {
+        "companies": companies
+    }
+
+    return render(request, 'index.html', context)
