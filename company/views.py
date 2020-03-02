f82e1f1d545459ae0a1eb4e7496bdaedde6423e9
NicolasDutour <dutourn99@gmail.com>
2020-03-02 16:53:36 +0100

    add pagination

--- company/views.py
+++ company/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Company
 import json
 
@@ -12,6 +13,17 @@ with open('mock_data.json', 'r') as f:
 
 def index(request):
     companies = Company.objects.all()
+    paginator = Paginator(companies, 8)
+
+    page = request.GET.get('page')
+    try:
+        companies = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        companies = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        companies = paginator.page(paginator.num_pages)
 
     context = {
         "companies": companies
